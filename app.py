ed495efded0c5a07c8e1892883d238e394e17d01
Katugwa Nathaniel <nkatugwa@gmail.com>
2026-03-25 07:04:16 +0100

    Serve public folder through Flask static route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 import math
 from functools import lru_cache
 
-app = Flask(__name__, template_folder="../templates", static_folder="public")
+app = Flask(__name__, template_folder="../templates", static_folder="public", static_url_path="")
 
 API_URL = "https://potterapi-fedeperin.vercel.app/en/characters"
 
